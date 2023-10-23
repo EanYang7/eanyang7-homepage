011ebe696284689c6503bcb3eb719265e845cec1
YQisme <1398757912@qq.com>
2023-10-23 16:41:37 +0800

    调试

--- home/next.config.js
+++ home/next.config.js
@@ -12,13 +12,23 @@ const nextConfig = {
         source: "/:path*",
         destination: `/:path*`,
       },
+      // 添加blog
       {
         source: "/blog",
-        destination: `${BLOG_URL}/blog`,
+        destination: `https://blog-psi-pied-75.vercel.app/blog`,
       },
       {
         source: "/blog/:path*",
-        destination: `${BLOG_URL}/blog/:path*`,
+        destination: `https://blog-psi-pied-75.vercel.app/blog/:path*`,
+      },
+      // 添加doc
+      {
+        source: "/doc",
+        destination: `https://doc-ten-lilac.vercel.app`,
+      },
+      {
+        source: "/doc/:path*",
+        destination: `https://doc-ten-lilac.vercel.app/:path*`,
       },
     ];
   },
